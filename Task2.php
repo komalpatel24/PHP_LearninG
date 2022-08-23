82c97b14acb781a64a66dba73ce1cffa290e3232
komalpatel24 <komalkantariya2001@gmail.com>
2022-08-23 19:06:00 +0530

    pattern

--- Task2.php
+++ Task2.php
@@ -7,27 +7,101 @@
     <title>Task2</title>
 </head>
 <body>
-    <?php
-     /* for($i=1;$i<=5;$i++){
-          for($j=0;$j<$i;$j++){
-      echo "*";
-          }
-      echo "<br>";
-      }*/
-
-      $list=array("*","#","@","$","^");
-        for($a=0; $a<=4; $a++){
-            echo "<br>";
-            for($b=0; $b<=$a; $b++)
-            {
-                
-                echo $list[$a];
-                
+       <?php
+            for($i=1;$i<=6;$i++){
+                for($j=1;$j<$i;$j++){
+            echo " $j ";
                 }
+            echo "<br>";
+            }
+            echo "<br>";
+         ?>     
+
+            <?php
+                    for($i=1; $i<=5; $i++){
+                        for($j=1; $j<=$i; $j++){
+                            echo $j % 2;
+                    }  echo "<br>";     
+                }
+                echo "<br>";
+             ?>
+
+          
+
+            <?php
+
+                    $rows = 5;
+                    $n = 1;
+                    for($i = 1; $i<=$rows; $i++){
+                        for($j=1; $j<=$i; $j++){
+                            echo $n, "&nbsp;&nbsp;";
+                            $n++;
+                        }
+                        echo "<br>";
+                    }
+                    echo "<br>";
+            ?>
+
+        <?php
+                $str="ANGEL";
+                $count=strlen($str);
+                for($i=0; $i<$count; $i++){
+                    for($j=0; $j<=$i; $j++){
+                        echo "  $str[$j]  ";
+                    }
+                    echo "<br>";
+                }
+                echo "<br>";
+            ?>
+
+
+        <?php  
+                for($i=1;$i<=5;$i++){
+                    for($j=0;$j<$i;$j++){
+                echo "  *  ";
+                    }
+                echo "<br>";
+                }
+                echo "<br>";
+           ?> 
+
+         <?php  
+                for($i=1;$i<=5;$i++){
+                    for($j=5;$j>=$i;$j--){
+                echo "  *  ";
+                    }
+                echo "<br>";
+                }
+                echo "<br>";
+           ?> 
+
+
+
+           <?php
+
+                $list=array(" * "," # "," @ "," $ "," ^ ");
+                    for($a=0; $a<=4; $a++){
+                        echo "<br>";
+                        for($b=0; $b<=$a; $b++)
+                        {
+                            echo $list[$a];
+                         }
+                    }
+                echo "<br><br>";
+             ?>
+    
+    <?php
+        $n=4;
+        for($i=1; $i<=$n; $i++){
+            for($j=1; $j<=(2*$n)-1; $j++){
+                if($j>=$n-($i-1) && $j<=$n+($i-1)){
+                    echo "*";
+                } else{
+                    echo "&nbsp;&nbsp;";
+                }
+            }
+            echo "<br>";
         }
-
-
-
     ?>
 </body>
 </html>
--- Task3.php
+++ Task3.php
@@ -7,15 +7,31 @@
     <title>Task3</title>
 </head>
 <body>
-    <?php
-     for($i=1;$i<=6;$i++){
-        for($j=1;$j<$i;$j++){
-    echo "$j";
-        }
-    echo "<br>";
-    }
+    
+<?php
+            for($i=5;$i>0;$i++){
+                for($j=5;$j>$i;$j++){
+            echo " $j ";
+                }
+            echo "<br>";
+            }
+            echo "<br>";
+         ?>     
 
-   
+<?php
+        $n=4;
+        for($i=1; $i<=$n; $i++){
+            for($j=1; $j<=(2*$n)-1; $j++){
+                if($j>=$n-($i-1) && $j<=$n+($i-1)){
+                    echo "*";
+                } else{
+                    echo "&nbsp;&nbsp;";
+                }
+            }
+            echo "<br>";
+        }
     ?>
+
+       
 </body>
 </html>                       
